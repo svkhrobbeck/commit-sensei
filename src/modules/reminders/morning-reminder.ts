646f9d147ebb0e5dfac818caa370246202a38b16
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-19 23:29:23 +0500

    feat: :sparkles: add changes

--- src/modules/reminders/morning-reminder.ts
+++ src/modules/reminders/morning-reminder.ts
@@ -5,7 +5,7 @@ import { weekDays } from "../../helpers/constants";
 
 const morningReminder = async () => {
   const currentDate = new Date();
-  const date = formatDate(currentDate, "DD-MMMM, YYYY");
+  const date = formatDate(currentDate, "DD-MM, YYYY");
 
   if (currentDate.getDay() === 1) {
     const deadline = await getDeadline();
@@ -13,7 +13,7 @@ const morningReminder = async () => {
     const message = dedent`
     #morning_reminder #new_week
     
-    Bugun ${date}-yil, yangi hafta boshlanmoqda, hafta uchun grafik:
+    Bugun ${date}, yangi hafta boshlanmoqda, hafta uchun grafik:
 
     <b>${deadline.map(item => `${weekDays[item.id]}: ${item.total} ta`).join("\n")}</b>
     commit yozishingiz kerak!!
@@ -27,7 +27,7 @@ const morningReminder = async () => {
     const message = dedent`
     #morning_reminder
     
-    Bugun ${date}-yil
+    Bugun ${date},
     Bugun kamida 7 ta commit yozishingiz kerak. Commitlarni hisoblashda haftadagi jarimalar hisobga olinadi.`;
 
     await notifyDevelopers(message, false);
