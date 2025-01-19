f3eb8c6accc8d7e7560f902603d9945ce5bfc087
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-19 23:25:58 +0500

    feat: :sparkles: add changes

--- src/start.ts
+++ src/start.ts
@@ -16,12 +16,7 @@ bot.start({
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
 
-nightReminder();
-// cron schedules
 cron.schedule("0 7 * * *", morningReminder);
 cron.schedule("0 14 * * *", afternoonReminder);
 cron.schedule("0 20 * * *", eveningReminder);
 cron.schedule("45 23 * * *", nightReminder);
-
-// 23:45 - bugun agar 7 tadan kam yozgan bo'lsa 7 - bugungi_commitlar_soni = qoldiq = jarima(googlesheets)
-// har yakshanba / haftadagi statistika
