9a0b69951157c5f4cca525f1492d4da68f979672
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-20 15:18:52 +0500

    feat: :sparkles: add changes

--- src/start.ts
+++ src/start.ts
@@ -2,17 +2,17 @@ import "dotenv/config";
 import cron from "node-cron";
 
 import app from "./server";
-import bot from "./bot/core";
+// import bot from "./bot/core";
 
 import { afternoonReminder, eveningReminder, morningReminder, nightReminder } from "./modules/reminders";
 
 const PORT = process.env.PORT!;
 
-bot.start({
-  onStart(info) {
-    console.log(`https://t.me/${info.username} has been started`);
-  },
-});
+// bot.start({
+//   onStart(info) {
+//     console.log(`https://t.me/${info.username} has been started`);
+//   },
+// });
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
 
