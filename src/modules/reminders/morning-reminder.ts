0b68b00ad02697dd72a94d2af67eaf0b9fb11232
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-21 10:49:30 +0500

    feat: :sparkles: make better morning reminder

--- src/modules/reminders/morning-reminder.ts
+++ src/modules/reminders/morning-reminder.ts
@@ -1,14 +1,22 @@
 import dedent from "dedent";
+import { getDeadline, getSetting } from "../sheets";
 import { formatDate, notifyDevelopers } from "../../utils";
 
 const morningReminder = async () => {
-  const date = formatDate(new Date(), "DD-MM, YYYY");
+  const dateInstance = new Date();
+  const date = formatDate(dateInstance, "DD-MM, YYYY");
+  const deadline = await getDeadline();
+  const setting = await getSetting();
+
+  const singleDeadline = deadline.find(item => item.id === dateInstance.getDay());
 
   const message = dedent`
     #morning_reminder
     
     Bugun ${date},
-    Bugun kamida 7 ta commit yozishingiz kerak. Commitlarni hisoblashda haftadagi jarimalar hisobga olinadi.`;
+    Bugun kamida ${
+      singleDeadline!.total || setting.avarageDailyCommitCount
+    } ta commit yozishingiz kerak. Commitlarni hisoblashda o'tgan haftadagi jarimalar hisobga olinadi.`;
 
   await notifyDevelopers(message, false);
 };
