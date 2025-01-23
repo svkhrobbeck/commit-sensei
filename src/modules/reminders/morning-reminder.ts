3a8197a734ca2a1f5c4db1036ed1b69b7330341e
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-23 20:59:16 +0500

    feat: :sparkles: refactor morning reminder

--- src/modules/reminders/morning-reminder.ts
+++ src/modules/reminders/morning-reminder.ts
@@ -3,17 +3,17 @@ import { getDeadline, getSetting } from "../sheets";
 import { formatDate, notifyDevelopers } from "../../utils";
 
 const morningReminder = async () => {
-  const dateInstance = new Date();
-  const date = formatDate(dateInstance, "DD-MM, YYYY");
+  const date = formatDate(new Date(), "DD-MM-YYYY");
   const deadline = await getDeadline();
   const setting = await getSetting();
 
-  const singleDeadline = deadline.find(item => item.id === dateInstance.getDay());
+  const singleDeadline = deadline.find(item => item.date === date);
 
   const message = dedent`
     #morning_reminder
     
-    Bugun ${date},
+    ${date}
+    
     Bugun kamida ${
       singleDeadline!.total || setting.averageDailyCommitCount
     } ta commit yozishingiz kerak. Commitlarni hisoblashda o'tgan haftadagi jarimalar hisobga olinadi.`;
