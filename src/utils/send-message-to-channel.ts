ec15471a6b744fced0a2ab3b8ad5468a655e7f41
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-26 23:47:08 +0500

    feat: :sparkles: add github account link to button

--- src/utils/send-message-to-channel.ts
+++ src/utils/send-message-to-channel.ts
@@ -5,10 +5,9 @@ import bot from "../bot/core";
 const channelId = process.env.CHANNEL_ID!;
 
 const sendMessageToChannel = async (message: string, withButton: boolean = true) => {
-  const inlineButton = new InlineKeyboard().url(
-    "Natijalarni jadvalda kuzatish",
-    `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`
-  );
+  const inlineButton = new InlineKeyboard()
+    .url("Natijalarni jadvalda kuzatish", `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`)
+    .url("Github akkauntingiz", `https://github.com/${process.env.GITHUB_USERNAME}`);
 
   if (withButton) {
     await bot.api.sendMessage(channelId, message, { parse_mode: "HTML", reply_markup: inlineButton });
