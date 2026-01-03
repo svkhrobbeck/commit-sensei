dc7f9d7d0707825d24b27b3dbb74f1091808b544
svkhrobbeck <svkhrobbeck@gmail.com>
2026-01-03 19:07:05 +0500

    refactor: :fire: remove unnecessary called function

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 
 import bot from "@/bot/core";
 import { runServer } from "@/app";
-import { afternoonReminder, eveningReminder, getStats, morningReminder, nightReminder } from "@/modules/reminders";
+import { afternoonReminder, eveningReminder, morningReminder, nightReminder } from "@/modules/reminders";
 
 bot.start({
   onStart(info) {
@@ -16,4 +16,3 @@ cron.schedule("0 7 * * *", morningReminder);
 cron.schedule("0 14 * * *", afternoonReminder);
 cron.schedule("58 19 * * *", eveningReminder);
 cron.schedule("58 23 * * *", nightReminder);
-getStats();
